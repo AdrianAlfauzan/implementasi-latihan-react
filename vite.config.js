4d52e2433e4aad1ea19dfcd45d390583a3916734
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-02-12 22:46:09 +0700

    coba

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/implementasi-learning-react-Router-Routes-Route/", // Sesuaikan dengan repo GitHub
   plugins: [react()],
+  base: "/implementasi-learning-react-Router-Routes-Route/", // Sesuaikan dengan repo GitHub
 });
