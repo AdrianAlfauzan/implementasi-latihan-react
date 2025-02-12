a26ef8c3503e9c5d6d5ecf16812a5c5ace768df0
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-02-12 23:09:43 +0700

    coba

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,6 @@ const NotFound = () => {
     <div>
       <div className="h-screen text-center text-xl flex flex-col items-center justify-center text-black bg-slate-700">
         <h1>TIDAK ADA!</h1>
-        <div>
-          <a href="/beranda">BERANDAss</a>
-        </div>
       </div>
     </div>
   );
